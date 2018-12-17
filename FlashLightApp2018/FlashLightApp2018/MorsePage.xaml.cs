c70a09bd02a8f2628b44444a7ecee3d410c943a4
Daniel Hano <38525633+danihano@users.noreply.github.com>
2018-12-17 15:49:36 +0000

    Added more buttons to MorsePage for the later implamented functions.

--- FlashLightApp2018/FlashLightApp2018/MorsePage.xaml.cs
+++ FlashLightApp2018/FlashLightApp2018/MorsePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +16,40 @@ namespace FlashLightApp2018
 		{
 			InitializeComponent ();
 		}
-	}
+        
+        private async void NewMorseBtn_Clicked(object sender, EventArgs e)
+        {
+            OBtn.IsEnabled = true;
+            LBtn.IsEnabled = true;
+            SBtn.IsEnabled = true;
+
+            //File.WriteAllText(fileName, text);
+
+        }
+
+        private void OBtn_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadMorseBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayMorseBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
