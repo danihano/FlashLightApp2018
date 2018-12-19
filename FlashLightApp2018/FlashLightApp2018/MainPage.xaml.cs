20f034074eba96b58fff030792068e03ebf7272e
Daniel Hano <38525633+danihano@users.noreply.github.com>
2018-12-19 14:38:51 +0000

    Added in inbedded Morse code onto morse code page.

--- FlashLightApp2018/FlashLightApp2018/MainPage.xaml.cs
+++ FlashLightApp2018/FlashLightApp2018/MainPage.xaml.cs
@@ -17,60 +17,25 @@ namespace FlashLightApp2018
 
         private async void OnBtn_Clicked(object sender, EventArgs e)
         {
-
             try
             {
-                //Turn on
-                //dot
+                //Turn off
                 await Flashlight.TurnOnAsync();
-                await Flashlight.TurnOffAsync();
-                //end dot
-
-                await Flashlight.TurnOnAsync();
-                await Flashlight.TurnOffAsync();
-
-                await Flashlight.TurnOnAsync();
-                await Flashlight.TurnOffAsync();
-
-                await Task.Delay(650);
-
-                //dash
-                await Flashlight.TurnOnAsync();
-                await Task.Delay(750);
-                await Flashlight.TurnOffAsync();
-                //end dash
-
-                await Flashlight.TurnOnAsync();
-                await Task.Delay(750);
-                await Flashlight.TurnOffAsync();
-
-                await Flashlight.TurnOnAsync();
-                await Task.Delay(750);
-                await Flashlight.TurnOffAsync();
-
-                await Task.Delay(650);
-                await Flashlight.TurnOnAsync();
-                await Flashlight.TurnOffAsync();
-
-                await Flashlight.TurnOnAsync();
-                await Flashlight.TurnOffAsync();
-
-                await Flashlight.TurnOnAsync();
-                await Flashlight.TurnOffAsync();
             }
-            catch(FeatureNotSupportedException fnsEx)
+            catch (FeatureNotSupportedException fnsEx)
             {
                 //Handle not supported on device exception
             }
-            catch(PermissionException pEx)
+            catch (PermissionException pEx)
             {
                 //Handle permission exception
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 //Unable to turn on/off flashlight
             }
         }
+    
 
         private async void OffBtn_Clicked(object sender, EventArgs e)
         {
@@ -93,14 +58,14 @@ namespace FlashLightApp2018
             }
         }
 
-        private async void MorseBtn_Clicked(object sender, EventArgs e)
+        private void MorseBtn_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new MorsePage());
+            Navigation.PushAsync(new MorseCode());
         }
 
-        private void LightStr_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new LightStrength());
-        }
+        //private void LightStr_Clicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushAsync(new LightStrength());
+        //}
     }
 }
---
+++ FlashLightApp2018/FlashLightApp2018/MorseCode.xaml.cs
@@ -0,0 +1,549 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlashLightApp2018
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MorseCode : ContentPage
+	{
+		public MorseCode ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void SOSBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //Turn on
+                //dot
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+                //end dot
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                //dash
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+                //end dash
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+
+        private async void HelloBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+
+        private async void WarningBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+
+        private async void WOWBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+
+        private async void LOLBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+
+        private async void EmergancyBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+
+        private async void ByeBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+
+        private async void GMITBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Flashlight.TurnOnAsync();
+                await Flashlight.TurnOffAsync();
+
+                await Task.Delay(650);
+
+                await Flashlight.TurnOnAsync();
+                await Task.Delay(700);
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                //Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                //Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                //Unable to turn on/off flashlight
+            }
+        }
+    }
+}
